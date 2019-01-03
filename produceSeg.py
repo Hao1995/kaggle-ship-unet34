822edcc8c4947c2275356270e6b6c373e6cf354f
hao <y26704325@gmail.com>
2019-01-03 15:40:20 +0800

    fixed

--- produceSeg.py
+++ produceSeg.py
@@ -4,6 +4,27 @@ import os
 
 from PIL import Image
 
+def rle_encode(img):
+    '''
+    not yet fixed
+    '''
+    # pixels = img.flatten()
+    # pixels = np.concatenate([[0], pixels, [0]])
+    # runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    # runs[1::2] -= runs[::2]
+    # return ' '.join(str(x) for x in runs)
+ 
+def rle_decode(mask_str, shape):
+    s = mask_str.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    
+    return img.reshape(shape)
+    
 maskDir = 'data/mask'
 try:
     os.makedirs(maskDir)
