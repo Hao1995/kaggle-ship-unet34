1a129b940a9f844b02f967a2491d77aa1541a407
hao <y26704325@gmail.com>
2019-01-05 19:29:10 +0800

    lab412-cpu test

--- train.py
+++ train.py
@@ -3,20 +3,24 @@ import LoadBatches
 import os
 import tensorflow as tf
 
-train_images_path = 'data/train/'
-train_segs_path = 'data/label/'
+train_images_path = 'data/train/1img/'
+train_segs_path = 'data/label/1img/'
 train_batch_size = 1
 n_classes = 2
 
-input_height = 384
-input_width = 384
-output_height = 384
-output_width = 384
+# input_height = 384
+# input_width = 384
+# output_height = 384
+# output_width = 384
+input_height = 768
+input_width = 768
+output_height = 768
+output_width = 768
 
 validate = False
 save_weights_path = 'results/'
 # epochs = 100
-EPOCHS = 100
+EPOCHS = 5
 STEPS_PER_EPOCH = 5
 optimizer_name = 'adam'
 
@@ -49,28 +53,33 @@ print("Model output shape", model.output_shape)
 model.summary()
 
 # === 保存和恢復模型 ===
-checkpoint_path = "training_callback/cp-{epoch:04d}-{val_acc:.2f}.ckpt"
-# checkpoint_path = "training_callback/cp.hdf5"
-checkpoint_dir = os.path.dirname(checkpoint_path)
+import os
+from datetime import datetime
 
-# Create checkpoint callback
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+checkpoint_path = "training_callback" + "/" + datetime.now().strftime('%Y%m%d%H%M%S')
+try:
+    os.makedirs(checkpoint_path)
+except:
+    print('Make directory', checkpoint_path, 'happend error.')
 
-# if os.path.isfile(checkpoint_path):
-#     model.load_weights(checkpoint_path)
-#     print("Load last time weigths file.")
-# ==========================
+checkpoint_file = checkpoint_path + "/weights-epoch-{epoch:04d}-acc-{acc:.2f}.hdf5"
 
+if os.path.isfile(checkpoint_file):
+    model.load_weights(checkpoint_file)
+
+from keras.callbacks import ModelCheckpoint
+cp_callback = ModelCheckpoint(checkpoint_file, monitor='acc',save_weights_only=True, verbose=1)
+
+# === Check length of input image is same with length of seg images ===
 TRAIN_IMG_NUM = len([name for name in os.listdir(train_images_path) if os.path.isfile(os.path.join(train_images_path, name))])
 TRAIN_SEG_NUM = len([name for name in os.listdir(train_segs_path) if os.path.isfile(os.path.join(train_segs_path, name))])
 if TRAIN_IMG_NUM != TRAIN_SEG_NUM:
     raise Exception('TRAIN_IMG_NUM = ', TRAIN_IMG_NUM , ' ; TRAIN_SEG_NUM = ', TRAIN_SEG_NUM, '. Is not equal.')
 
+# === Make a image generator ===
+
 G = LoadBatches.imageSegmentationGenerator(train_images_path, train_segs_path, train_batch_size, n_classes,
                                            input_height, input_width, output_height, output_width)
-
 if validate:
     G2 = LoadBatches.imageSegmentationGenerator(val_images_path, val_segs_path, val_batch_size, n_classes, input_height,
                                                 input_width, output_height, output_width)
@@ -94,7 +103,8 @@ if not validate:
     # model.save(save_weights_path + "model_steps" + str(STEPS_PER_EPOCH) + ".h5")
 
     # === Epochs 
-    history = model.fit_generator(G, TRAIN_IMG_NUM//train_batch_size, epochs=EPOCHS, callbacks=[cp_callback])
+    # history = model.fit_generator(G, TRAIN_IMG_NUM//train_batch_size, epochs=EPOCHS, callbacks=[cp_callback])
+    model.fit_generator(G, TRAIN_IMG_NUM//train_batch_size, epochs=EPOCHS, callbacks=[cp_callback])
     # model.fit_generator(G, 1//train_batch_size, epochs=EPOCHS)
     model.save_weights(save_weights_path + "model_epochs." + str(EPOCHS))
     model.save(save_weights_path + "model_epochs" + str(EPOCHS) + ".h5")
@@ -102,24 +112,3 @@ else:
     model.fit_generator(G, steps_per_epoch=STEPS_PER_EPOCH, validation_data=G2, validation_steps=200)
     model.save_weights(save_weights_path + "model_steps." + str(STEPS_PER_EPOCH))
     model.save(save_weights_path + "model_steps" + str(STEPS_PER_EPOCH) + ".h5")
-
-from keras.utils import plot_model
-plot_model(model, to_file='model.png')
-
-print(history.history.keys())
-
-fig = plt.figure()
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='lower left')
-#
-fig.savefig('performance.png')
