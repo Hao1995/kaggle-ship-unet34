9e056dff1513ffa9c3a7dc2e9933ce3437b5135a
hao <y26704325@gmail.com>
2019-01-06 14:27:17 +0800

    test

--- Models.py
+++ Models.py
@@ -106,6 +106,7 @@ def dice_coef(y_true, y_pred, smooth=1):
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
 
+
 def dice_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
 
