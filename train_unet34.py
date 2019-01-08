86b1f4c74c3d09b8d230cbc45c036c1385fe1bc9
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2019-01-08 11:04:51 +0800

    fixed path

--- train_unet34.py
+++ train_unet34.py
@@ -175,5 +175,5 @@ history = model.fit_generator(gen,
                     epochs=epochs,
                     callbacks=[model_checkpoint])
 
-model.save_weights(save_weights_path + "model_epochs." + str(epoch))
-model.save(save_weights_path + "model_epoch" + str(epoch) + ".h5")
+model.save_weights(SAVE_WEIGHTS_PATH + "model_epochs." + str(epoch))
+model.save(SAVE_WEIGHTS_PATH + "model_epoch" + str(epoch) + ".h5")
