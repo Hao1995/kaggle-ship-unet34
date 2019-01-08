4216ae04bb710a1a03e45617f02836b8ed92a776
LAPTOP-GJE59E32\user <y26704325@gmail.com>
2019-01-08 12:19:50 +0800

    add path

--- train_unet34_param.py
+++ train_unet34_param.py
@@ -2,7 +2,10 @@ IMG_PATH = 'data/train/'
 SEG_PATH = 'data/label/'
 SAVE_WEIGHTS_PATH = 'results/'
 
-img_size_ori = 768
-img_size_target = 768
+TEST_IMG_PATH = 'data/test/'
+SEG_RESULT_PATH = 'data/seg_results/'
+
+img_size_ori = 384
+img_size_target = 384
 batch_size = 2 # 256:64, 384:32, 768:6(8)
-epochs = 20
+epochs = 1
